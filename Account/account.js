1193c0d5b13ffdbbe02a13f0c75385f6b789e6e6
Replit user <>
2023-06-09 17:17:19 +0700

    Added some more file

---
+++ Account/account.js
@@ -0,0 +1,7 @@
+let buttonLoader = document.getElementsByName("sign-in-3rd")
+
+buttonLoader.forEach(element => {
+    element.addEventListener("click", ()=>{
+        alert("Sorry, this feature comming soon!")
+    })
+});
